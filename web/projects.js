31f4722c30540cd30bd1901c924085e7edcc737d
mastersrp <tcg.thegamer@gmail.com>
2013-05-13 14:37:23 +0200

    Fix various references to mastersrp.kodingen.com. Remove contact page file. It will be accessible under the 'About' tab. Generate project page instead of hardcode. (projects.js) Fix blog link pointing to non-existant site.

---
+++ web/projects.js
@@ -0,0 +1,13 @@
+function projectCtrl( $scope ) {
+				$scope.projects = [
+				{
+								"name":"PyUpdater",
+								"url":"http://github.com/mastersrp/PyUpdater",
+								"desc": "A package manager + compiler system. Currently still work in progress, but hasn't been worked on due to the above project. (requires Python 3)"
+				},{
+								"name":"hacktheplanet",
+								"url":"http://github.com/mastersrp/hacktheplanet",
+								"desc":"A hacking simulator/game. Supposed to be somewhat of a Uplink-esque realistic game."
+				}
+				];
+}
